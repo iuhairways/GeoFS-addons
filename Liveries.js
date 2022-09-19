b5e2465417f1b95dd44561b97ef1aa4ea3e8c472
iuhairways <103064103+iuhairways@users.noreply.github.com>
2022-09-19 17:02:18 +0100

    Update Liveries.js

--- Liveries.js
+++ Liveries.js
@@ -9,13 +9,13 @@ async function init(){
     // Panel for list
     
     let listdiv = document.createElement("div");
-    listdiv.innerHTML = '<ul data-noblur="true" data-onshow="{geofs.initializePreferencesPanel()}" data-onhide="{geofs.savePreferencesPanel()}" class="geofs-list geofs-toggle-panel geofs-autoland-list geofs-preferences"><h3>Livery Selector</h3><div class="mdl-textfield mdl-js-textfield geofs-stopMousePropagation geofs-stopKeyupPropagation" style="width: 100%; padding-right: 86px;"><input class="mdl-textfield__input address-input" type="text" id="address" placeholder="Search liveries" onkeydown="search(this.value)" id="searchlivery"><label class="mdl-textfield__label" for="searchlivery">Search liveries</label></div><h6>Favorite liveries</h6><ul id="favorites" class="geofs-list geofs-visible"></ul><h6>Available liveries</h6><ul id="liverylist" class=" geofs-list geofs-visible"></ul></ul>'
+    listdiv.innerHTML = '<ul data-noblur="true" data-onshow="{geofs.initializePreferencesPanel()}" data-onhide="{geofs.savePreferencesPanel()}" class="geofs-list geofs-toggle-panel geofs-autoland-list geofs-preferences"><h3>Liveries</h3><div class="mdl-textfield mdl-js-textfield geofs-stopMousePropagation geofs-stopKeyupPropagation" style="width: 100%; padding-right: 86px;"><input class="mdl-textfield__input address-input" type="text" id="address" placeholder="Search" onkeydown="search(this.value)" id="searchlivery"><label class="mdl-textfield__label" for="searchlivery">Search</label></div><h6>Starred</h6><ul id="favorites" class="geofs-list geofs-visible"></ul><h6>For this aircraft</h6><ul id="liverylist" class=" geofs-list geofs-visible"></ul></ul>'
 
     document.getElementsByClassName("geofs-ui-left")[0].appendChild(listdiv);
 
     // Button for panel
     let buttonDiv = document.createElement("div");
-    buttonDiv.innerHTML = '<button class="mdl-button mdl-js-button geofs-f-standard-ui geofs-mediumScreenOnly" data-toggle-panel=".geofs-autoland-list" data-tooltip-classname="mdl-tooltip--top" id="liverybutton" tabindex="0" data-upgraded=",MaterialButton" onclick="listLiveries()">LIVERY</button>' //onclick="listLiveries()"
+    buttonDiv.innerHTML = '<button class="mdl-button mdl-js-button geofs-f-standard-ui geofs-mediumScreenOnly" data-toggle-panel=".geofs-autoland-list" data-tooltip-classname="mdl-tooltip--top" id="liverybutton" tabindex="0" data-upgraded=",MaterialButton" onclick="listLiveries()">Liveries</button>' //onclick="listLiveries()"
     document.body.appendChild(buttonDiv);
     document.getElementsByClassName("geofs-ui-bottom")[0].appendChild(buttonDiv);
     let element = document.getElementById("liverybutton");
@@ -81,7 +81,18 @@ function listLiveries(){
     let mode = liveryobj.aircrafts[airplane].mode;
     let parts = liveryobj.aircrafts[airplane].parts;
 
-   
+    liveryobj.aircrafts[airplane].liveries.forEach(function(e){
+        var dropdown = document.createElement('li');
+        dropdown.innerHTML = e.name;
+        let star = document.createElement("span");
+        star.setAttribute("class", "fa fa-star nocheck");
+        star.setAttribute("id", geofs.aircraft.instance.id + "_" + e.name);
+        star.setAttribute("onclick", "star(this)");
+        dropdown.appendChild(star);
+        dropdown.style.display = "block";
+        dropdown.setAttribute("id", geofs.aircraft.instance.id + "_" + e.name + "_button");
+        document.getElementById("liverylist").appendChild(dropdown);
+        dropdown.setAttribute("onclick", "loadLivery('"+ e.texture +"', "+ mode +", ["+ parts +"])")
     })
     sortList("liverylist");
     loadFavorites();
