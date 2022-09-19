23103f2de5ced9c279908bae4e450de1bfc5e34f
iuhairways <103064103+iuhairways@users.noreply.github.com>
2022-09-19 08:09:13 +0100

    Update MainTest.js

--- MainTest.js
+++ MainTest.js
@@ -1,76 +1,178 @@
-console.log("Loading...");
-let livObj;
- await fetch("https://raw.githubusercontent.com/iuhairways/GeoFS-liveries/main/Liveries.json")
- .then(res => res.json())
- .then(data => livObj = data) 
-if (geofs.version == 2.9) { //should work with URLs and livery IDs now, legacy geofs compat.
-livObj.aircraft.forEach(function(e){
-  var dropdown = document.createElement('li');
-  dropdown.innerHTML = e.name;
-  document.getElementsByClassName("geofs-aircraft-list")[0].appendChild(dropdown);
-  if (e.livery.includes("https://")) {
-  dropdown.setAttribute("onclick", 'geofs.api.Model.prototype.changeTexture(' + `"` + e.livery + `"` + ', 0, geofs.aircraft.instance.definition.parts[0]["3dmodel"])');
-  } else {
-      dropdown.setAttribute("onclick", 'geofs.aircraft.instance.loadLivery(' + e.livery + ')');
-  }
-})
-  function updateMultiplayer(){
-  Object.values(multiplayer.visibleUsers).forEach(function(e){
-    a = e;
-    livObj.aircraft.forEach(function(e){
-      if (a.currentLivery == parseInt(e.livery)) {   geofs.api.Model.prototype.changeTexture(e.mptx, 0, multiplayer.visibleUsers[a.id].model);
-      }
+let liveryobj;
+let panelopen = 0;
+let multiplayertexture
+
+//init
+
+async function init(){
+
+    // Panel for list
+    
+    let listdiv = document.createElement("div");
+    listdiv.innerHTML = '<ul data-noblur="true" data-onshow="{geofs.initializePreferencesPanel()}" data-onhide="{geofs.savePreferencesPanel()}" class="geofs-list geofs-toggle-panel geofs-autoland-list geofs-preferences"><h3>Livery Selector</h3><div class="mdl-textfield mdl-js-textfield geofs-stopMousePropagation geofs-stopKeyupPropagation" style="width: 100%; padding-right: 86px;"><input class="mdl-textfield__input address-input" type="text" id="address" placeholder="Search liveries" onkeydown="search(this.value)" id="searchlivery"><label class="mdl-textfield__label" for="searchlivery">Search liveries</label></div><h6>Favorite liveries</h6><ul id="favorites" class="geofs-list geofs-visible"></ul><h6>Available liveries</h6><ul id="liverylist" class=" geofs-list geofs-visible"></ul></ul>'
+
+    document.getElementsByClassName("geofs-ui-left")[0].appendChild(listdiv);
+
+    // Button for panel
+    let buttonDiv = document.createElement("div");
+    buttonDiv.innerHTML = '<button class="mdl-button mdl-js-button geofs-f-standard-ui geofs-mediumScreenOnly" data-toggle-panel=".geofs-autoland-list" data-tooltip-classname="mdl-tooltip--top" id="liverybutton" tabindex="0" data-upgraded=",MaterialButton" onclick="listLiveries()">LIVERY</button>' //onclick="listLiveries()"
+    document.body.appendChild(buttonDiv);
+    document.getElementsByClassName("geofs-ui-bottom")[0].appendChild(buttonDiv);
+    let element = document.getElementById("liverybutton");
+    document.getElementsByClassName("geofs-ui-bottom")[0].insertBefore(element, buttonDiv);
+
+    let styles = document.createElement("div");
+    styles.innerHTML = '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/><style>.checked {text-shadow: 0 0 1px black, 0 0 1px black, 0 0 1px black, 0 0 1px black;color: rgb(255,193,7); display: inline; text align: right; cursor: pointer;}.nocheck {text-shadow: 0 0 1px black, 0 0 1px black, 0 0 1px black, 0 0 1px black;color: white; display: inline; text align: right; cursor: pointer;}</style>';
+    document.body.appendChild(styles);
+
+    //Load liveries
+
+
+    await fetch("https://raw.githubusercontent.com/iuhairways/GeoFS-liveries/main/Liveries.json").then(res => res.json()).then(data => liveryobj = data)
+
+
+    //remove original buttons
+
+    document.querySelectorAll('[data-livery]').forEach(function(e){
+        e.parentElement.removeChild(e);
     })
-  })
+
 }
-} else {
-	livObj.aircraft.forEach(function(e){
-  var dropdown = document.createElement('li');
-  dropdown.innerHTML = e.name;
-  document.getElementsByClassName("geofs-aircraft-list")[0].appendChild(dropdown);
-  if (e.livery.includes("https://")) {
-  dropdown.setAttribute("onclick", 'geofs.api.changeModelTexture(geofs.aircraft.instance.definition.parts[0]["3dmodel"]._model, ' + `"` + e.livery + `"` + ', 0)');
-  } else {
-      dropdown.setAttribute("onclick", 'geofs.aircraft.instance.loadLivery(' + e.livery + ')');
+
+function loadLivery(texture, mode, parts){
+    //change livery
+    parts.forEach(function(e){
+        if (geofs.version == 2.9) {
+            geofs.api.Model.prototype.changeTexture(texture, mode, geofs.aircraft.instance.definition.parts[e]["3dmodel"]);
+            } else {
+            geofs.api.changeModelTexture(geofs.aircraft.instance.definition.parts[e]["3dmodel"]._model, texture, mode);
+        }
+        //change multiplayer texture
+        multiplayertexture = texture;
+    });
+}
+
+function sortList(id) {
+    var list, i, switching, b, shouldSwitch;
+    list = document.getElementById(id);
+    switching = true;
+    while (switching) {
+      switching = false;
+      b = list.getElementsByTagName("LI");
+      for (i = 0; i < (b.length - 1); i++) {
+        shouldSwitch = false;
+        if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          break;
+        }
+      }
+      if (shouldSwitch) {
+        b[i].parentNode.insertBefore(b[i + 1], b[i]);
+        switching = true;
+      }
+    }
   }
-})
+
+function listLiveries(){
+    document.getElementById("liverylist").innerHTML = "";
+
+    let airplane = geofs.aircraft.instance.id;
+
+    let mode = liveryobj.aircrafts[airplane].mode;
+    let parts = liveryobj.aircrafts[airplane].parts;
+
+    liveryobj.aircrafts[airplane].liveries.forEach(function(e){
+        var dropdown = document.createElement('li');
+        dropdown.innerHTML = e.name;
+        let star = document.createElement("span");
+        star.setAttribute("class", "fa fa-star nocheck");
+        star.setAttribute("id", geofs.aircraft.instance.id + "_" + e.name);
+        star.setAttribute("onclick", "star(this)");
+        dropdown.appendChild(star);
+        dropdown.style.display = "block";
+        dropdown.setAttribute("id", geofs.aircraft.instance.id + "_" + e.name + "_button");
+        document.getElementById("liverylist").appendChild(dropdown);
+        dropdown.setAttribute("onclick", "loadLivery('"+ e.texture +"', "+ mode +", ["+ parts +"])")
+    })
+    sortList("liverylist");
+    loadFavorites();
+    sortList("favorites");
+}
+
+function search(text){
+    console.log("search");
+    if (text === ""){
+        listLiveries();
+    }
+
+    else {
+        var liveries = document.getElementById("liverylist").childNodes;
+        liveries.forEach(function(e){
+            if (e.innerText.toLowerCase().includes(text.toLowerCase())){
+                e.style.display = "block";
+            }
+            else {
+                e.style.display = "none";
+            }
+        })
+    }
+}
+
+function star(element){
+    let e = element.classList;
+    console.log(e);
+    console.log("clicked");
+    if (e == "fa fa-star nocheck"){
+        console.log("checked");
+        let btn = document.getElementById(element.id +"_button");
+        let fbtn = document.createElement("li");
+        fbtn.innerText = btn.innerText;
+        fbtn.setAttribute("id", element.id + "_favorite");
+        fbtn.setAttribute("onclick", btn.getAttribute('onclick'));
+        document.getElementById("favorites").appendChild(fbtn);
+        let list = localStorage.favorites.split(",");
+        list.push(element.id);
+        list = [...new Set(list)]
+        localStorage.favorites = list;
+
+    }
+    else if (e == "fa fa-star checked"){
+        console.log("checked out");
+        document.getElementById("favorites").removeChild(document.getElementById(element.id + "_favorite"));
+        let list = localStorage.favorites.split(",");
+        let index = list.indexOf(element.id);
+        if (index !== -1) {
+            list.splice(index, 1);
+        }
+        localStorage.favorites = list;
+    }
+    //style animation
+    e.toggle("checked");
+    e.toggle("nocheck");
+}
+
+function loadFavorites(){
+    document.getElementById("favorites").innerHTML = "";
+    let list = localStorage.favorites.split(",");
+    console.log(list);
+    let airplane =  geofs.aircraft.instance.id;
+    list.forEach(function(e){
+        console.log(e.slice(0, airplane.length));
+        if ((airplane == e.slice(0, airplane.length)) && (e.charAt(airplane.length) == "_")){
+            star(document.getElementById(e));
+            console.log(document.getElementById("favorites").innerHTML);
+        }
+    })
+}
+
 function updateMultiplayer(){
-  Object.values(multiplayer.visibleUsers).forEach(function(e){
-    a = e;
-    livObj.aircraft.forEach(function(e){
-      if (a.currentLivery == parseInt(e.livery)) {   geofs.api.changeModelTexture(multiplayer.visibleUsers[a.id].model, e.mptx, 0);
-      }
-    })
-  })
+    Object.values(multiplayer.visibleUsers).forEach(function(e){
+        geofs.api.changeModelTexture(multiplayer.visibleUsers[e.id].model, multiplayertexture, 0);
+        })
 }
-}
- mpRefresh = setInterval(function(){  
-updateMultiplayer();
+
+init();
+
+let refreshMultiplayer = setInterval(function(){  
+    updateMultiplayer();
 }, 5000)
-document.querySelectorAll('[data-aircraft]').forEach(function(e){
-   var elemName = e.outerText;
-    if (elemName.includes("Airbus a320neo (Iberia) (by Spice_9)") || elemName.includes("Boeing 737 Max 8 (TUI) (by Spice_9)") || elemName.includes("Boeing 787-10 Dreamliner (Etihad) (by Spice_9)") || elemName.includes("Airbus A319 (Finnair)  (by GT-VRA)")) {
-       e.innerHTML = e.innerHTML + " [Multiliveries Frame]"
-    }
-});
-console.log("Loaded!");
-let contributors;
-await fetch("https://raw.githubusercontent.com/Spice9/Geofs-Multiliveries/main/dependencies/contributors.txt")
-.then(res => res.json())
- .then(data => contributors = data)
-let message = ""
-setTimeout(function(){
-  console.log("Code by Spice9 and AriakimTaiyo, livery contributions by:");
-contributors.forEach(function(e){
-  if (message === "") {
-    message = message + e
-  } else {
-    if (contributors[contributors.length - 1] === e) {
-      message = message + ", and " + e;
-    } else {
-      message = message + ", " + e
-    }
-  }
-})
-console.log(message)
-}, 1000)
