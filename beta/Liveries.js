7c318b3011f6d4720477013678c405a55c8c3405
iuhairways <103064103+iuhairways@users.noreply.github.com>
2022-10-22 09:25:59 +0100

    Update Liveries.js

--- beta/Liveries.js
+++ beta/Liveries.js
@@ -9,13 +9,13 @@ async function init(){
     // Panel for list
     
     let listdiv = document.createElement("div");
-    listdiv.innerHTML = '<ul data-noblur="true" data-onshow="{geofs.initializePreferencesPanel()}" data-onhide="{geofs.savePreferencesPanel()}" class="geofs-list geofs-toggle-panel geofs-autoland-list geofs-preferences"><h3><img src="https://raw.githubusercontent.com/iuhairways/GeoFS-addons/main/adon%20logo.png" width="95%" title="GeoFS Addon Livery pack" style="display: block; margin-left: auto; margin-right: auto;"/></h3><div class="mdl-textfield mdl-js-textfield geofs-stopMousePropagation geofs-stopKeyupPropagation" style="width: 100%; padding-right: 86px;"><input class="mdl-textfield__input address-input" type="text" id="address" placeholder="Search for a livery..." onkeydown="search(this.value)" id="searchlivery"><label class="mdl-textfield__label" for="searchlivery">Search for a livery...</label></div><h6>Favorited liveries</h6><ul id="favorites" class="geofs-list geofs-visible"></ul><h6>For this aircraft</h6><ul id="liverylist" class=" geofs-list geofs-visible"></ul></ul>'
+    listdiv.innerHTML = '<ul data-noblur="true" data-onshow="{geofs.initializePreferencesPanel()}" data-onhide="{geofs.savePreferencesPanel()}" class="geofs-list geofs-toggle-panel geofs-autoland-list geofs-preferences"><h3> title="GeoFS Addon Livery pack" style="display: block; margin-left: auto; margin-right: auto;"/></h3><div class="mdl-textfield mdl-js-textfield geofs-stopMousePropagation geofs-stopKeyupPropagation" style="width: 100%; padding-right: 86px;"><input class="mdl-textfield__input address-input" type="text" id="address" placeholder="Search for a livery..." onkeydown="search(this.value)" id="searchlivery"><label class="mdl-textfield__label" for="searchlivery">Search for a livery...</label></div><h6>Favorited liveries</h6><ul id="favorites" class="geofs-list geofs-visible"></ul><h6>For this aircraft</h6><ul id="liverylist" class=" geofs-list geofs-visible"></ul></ul>'
 
     document.getElementsByClassName("geofs-ui-left")[0].appendChild(listdiv);
 
     // Button for panel
     let buttonDiv = document.createElement("div");
-    buttonDiv.innerHTML = '<button class="mdl-button mdl-js-button geofs-f-standard-ui geofs-mediumScreenOnly" data-toggle-panel=".geofs-autoland-list" data-tooltip-classname="mdl-tooltip--top" id="liverybutton" tabindex="0" data-upgraded=",MaterialButton" onclick="listLiveries()" title="GeoFS Addon liveries">liveries</button>'
+    buttonDiv.innerHTML = '<button class="mdl-button mdl-js-button geofs-f-standard-ui geofs-mediumScreenOnly" data-toggle-panel=".geofs-autoland-list" data-tooltip-classname="mdl-tooltip--top" id="liverybutton" tabindex="0" data-upgraded=",MaterialButton" onclick="listLiveries()" title="GeoFS Addon liveries">liveries<img src="https://raw.githubusercontent.com/iuhairways/GeoFS-addons/main/adon%20logo.png" height="30px"/></button>'
     document.body.appendChild(buttonDiv);
     document.getElementsByClassName("geofs-ui-bottom")[0].appendChild(buttonDiv);
     let element = document.getElementById("liverybutton");
